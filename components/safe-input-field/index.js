42797a3bf1f1af29f36cda9ad06a74e765283a3e
astghik <astghik.p100@gmail.com>
2024-09-30 17:33:05 +0400

    added range input parent calssname

--- components/safe-input-field/index.js
+++ components/safe-input-field/index.js
@@ -375,7 +375,7 @@ class SafeInputField extends React.Component {
             this.renderErrorMessage(),
             this.renderLabel(),
             helpElement ? helpElement : null,
-            React.createElement("span", null,
+            React.createElement("span", { className: `refero-${type}-input` },
                 prefix,
                 React.createElement("input", Object.assign({ ref: this.inputFieldRef, id: inputName, name: inputName, type: type ? type : 'text', value: inputValue, placeholder: placeholder, className: inputClasses, min: min, max: max, minLength: minLength, autoComplete: autocomplete || 'off', tabIndex: tabIndex, "data-testid": inputTestId, onChange: this.onChange, onBlur: this.onBlur, onFocus: this.onFocus, onMouseDown: this.onMouseDown, onKeyDown: onKeyDown, "aria-label": ariaLabel, "aria-labelledby": ariaLabelledby, "aria-required": ariaRequired || required, required: required, disabled: disabled, readOnly: readOnly }, inputProps, ariaInvalid)),
                 sufix,
